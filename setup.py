81a29c8c49a952171bbd5558258345ccd7935604
ToddHoward42069 <89554916+ToddHoward42069@users.noreply.github.com>
2024-06-03 22:09:14 +0200

    v4

--- setup.py
+++ setup.py
@@ -1,25 +1,14 @@
 # setup.py
-import sys
-from cx_Freeze import setup, Executable
-import os
-
-# Path to the DLL file
-dll_path = 'C:\\Windows\\System32\\VCRUNTIME140.dll'
-
-build_exe_options = {
-    "packages": ["os", "tkinter", "pytube", "PIL", "requests", "certifi"],
-    "excludes": [],
-    "include_files": [dll_path]
-}
-
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"  # Use this if you don't want a console window
+from distutils.core import setup
+import py2exe
 
 setup(
-    name="YouTubeDownloader",
-    version="0.1",
-    description="YouTube Video Downloader",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("youtube_downloader.py", base=base)]
+    console=['youtube_downloader.py'],
+    options={
+        'py2exe': {
+            'bundle_files': 1,  # Bundle everything into a single file
+            'compressed': True,
+        }
+    },
+    zipfile=None,
 )
